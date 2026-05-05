4028adfc60cd8e7fdc4df4888404eeee4a818968
Eph23 <ephraims.818@gmail.com>
2026-05-05 14:45:05 +0600

    REACT--ULTIMATE-COURSE - SECTION PROJECTS - PT1 - Pizza Menu -- 02. Dynamic Pizza Data

--- [PROJECTS - PT-1]/01-pizza-menu/src/App.jsx
+++ [PROJECTS - PT-1]/01-pizza-menu/src/App.jsx
@@ -1,3 +1,5 @@
+import PIZZAS from "./data.js";
+
 function Header() {
     return (
         <header className="header">
@@ -23,55 +25,23 @@ function Menu() {
 function Pizzas() {
     return (
         <ul className="pizzas">
-            <li className="pizza">
-                <img src="pizzas/focaccia.jpg" alt="" />
-                <div>
-                    <h3>Focaccia</h3>
-                    <p>Bread with italian olive oil and rosemary</p>
-                    <span>6</span>
-                </div>
-            </li>
-            <li className="pizza">
-                <img src="pizzas/margherita.jpg" alt="" />
-                <div>
-                    <h3>Pizza Margherita</h3>
-                    <p>Tomato and mozarella</p>
-                    <span>10</span>
-                </div>
-            </li>
-            <li className="pizza">
-                <img src="pizzas/spinaci.jpg" alt="" />
-                <div>
-                    <h3>Pizza Spinaci</h3>
-                    <p>Bread with italian olive oil and rosemary</p>
-                    <span>12</span>
-                </div>
-            </li>
-            <li className="pizza">
-                <img src="pizzas/funghi.jpg" alt="" />
-                <div>
-                    <h3>Pizza Funghi</h3>
-                    <p>Tomato, mozarella, mushrooms, and onion</p>
-                    <span>12</span>
-                </div>
-            </li>
-            <li className="pizza sold-out">
-                <img src="pizzas/salamino.jpg" alt="" />
-                <div>
-                    <h3>Pizza Salamino</h3>
-                    <p>Tomato, mozarella, and pepperoni</p>
-                    <span>SOLD OUT</span>
-                </div>
-            </li>
-            <li className="pizza">
-                <img src="pizzas/prosciutto.jpg" alt="" />
-                <div>
-                    <h3>Pizza Prosciutto</h3>
-                    <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-                    <span>18</span>
-                </div>
-            </li>
+            {PIZZAS.map((pizza) => (
+                <Pizza key={pizza.name} pizza={pizza} />
+            ))}
         </ul>
+    );
+}
+
+function Pizza({ pizza }) {
+    return (
+        <li className="pizza">
+            <img src={pizza.photoName} alt="" />
+            <div>
+                <h3>{pizza.name}</h3>
+                <p>{pizza.ingredients}</p>
+                <span>{pizza.price}</span>
+            </div>
+        </li>
     );
 }
 
